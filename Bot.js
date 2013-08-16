008e064275c468963c3f4c6dd7156de16e2f6158
Turntablelover <darthcoolness@gmail.com>
2013-08-16 18:01:16 -0400

    Updated Song Info Feature
    Updated:
    1. Song Info Feature

--- Bot.js
+++ Bot.js
@@ -2659,4 +2659,5 @@ bot.on('newsong', function (data) {
 bot.speak('The current song is: ' + data.room.metadata.current_song.metadata.song);
 bot.speak('From the artist: ' + data.room.metadata.current_song.metadata.artist);
 bot.speak('From the album: ' + data.room.metadata.current_song.metadata.album);
+bot.speak('Genre is: ' + data.room.metadata.current_song.metadata.genre);
 });
